a53af4b80b24616006f9c9e580b6e46b73b35f01
Dennis Rönn <dennisronn@hotmail.com>
2021-02-02 15:44:20 +0100

    - Comments, comments everywhere

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+// Timer implementation
 function Timer() {
     this.start_ = new Date()
 
@@ -10,6 +11,7 @@ function Timer() {
     }
 }
 
+// Checks two cookies are equal
 function cookieMatch(c1, c2) {
     return (c1.name == c2.name) && (c1.domain == c2.domain) &&
             (c1.hostOnly == c2.hostOnly) && (c1.path == c2.path) &&
@@ -17,6 +19,7 @@ function cookieMatch(c1, c2) {
             (c1.session == c2.session) && (c1.storeId == c2.storeId);
 }
 
+// Sorts an array of keys
 function sortedKeys(array) {
     const keys = []
     for (let i in array) {
@@ -26,6 +29,7 @@ function sortedKeys(array) {
     return keys
 }
 
+// Cookie cache class, provided uncommented 'as is'
 function CookieCache() {
     this._cookies = {}
     this.reset = function() {
@@ -67,38 +71,46 @@ function CookieCache() {
     }
 }
 
+// State actions class, calls callbacks for boolean values when the state changes
 function StateActions(falseCallback, trueCallback) {
     this._onFalse = falseCallback
     this._onTrue = trueCallback
     this._state = false
 
+    // Set the new state
     this.set = function(state) {
         state = !!state
         const changed = state != this._state
         this._state = state
 
+        // If the state is changed, use the callback
         if (changed) {
+            // Pick the callback that represent the new state
             const cb = (state ? this._onTrue : this._onFalse)
             if (typeof(cb) === "function") {
                 cb()
             }
         }
 
+        // Return the current state (the new state)
         return this._state
     }
 
+    // Toggles the state
     this.toggle = function() {
         return this.set(!this._state)
     }
 
+    // Gets the current state
     this.get = function() {
         return !!this._state
     }
 }
 
-
+// Instantiate the cache variable
 var cache = new CookieCache()
 
+// Listener for new or updated cookies
 function listener(info) {
     cache.remove(info.cookie)
     if (!info.removed) {
@@ -106,25 +118,33 @@ function listener(info) {
     }
 }
 
+// Run from StateActions when turned on
 function startListening() {
     chrome.cookies.onChanged.addListener(listener)
 }
 
+// Run from StateActions when turned off
 function stopListening() {
     chrome.cookies.onChanged.removeListener(listener)
 }
 
+// Instantiate state actions with the correct callbacks
 var listenState = new StateActions(stopListening, startListening)
 
+// Load the initial cookies
 function loadCookies(cookies) {
+    // Turns the listener on, using StateActions to make sure it's not turned on twice
     listenState.set(true)
+    // Add each cookie to the cache
     cookies.forEach(p => cache.add(p))
 }
 
+// Looks for the username cookie and returns the username, or null if not found
 function getUserName() {
     return cache.getCookies(".twitch.tv").filter(p => (p.name === "name" || p.name === "login") && (typeof(p.value) === "string" && p.value.length > 0)).map(p => p.value)[0] || null
 }
 
+// Fetches all initial cookies and registers listeners
 function onload() {
     chrome.cookies.getAll({}, loadCookies)
     chrome.runtime.onMessage.addListener(messageHandler)
@@ -132,6 +152,7 @@ function onload() {
     chrome.action.onClicked.addListener(actionClickHandler)
 }
 
+// Class for messages
 function RequestMessage(request, sender, sendResponse) {
     this.label = request.label
     this.request = request
@@ -140,20 +161,30 @@ function RequestMessage(request, sender, sendResponse) {
     this.result = undefined
 }
 
+// The message handler used when messages arrive to service worker
 function messageHandler(request, sender, sendResponse) {
+    // Log who sent the message
     console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension")
 
+    // Check if the label is for a valid message and that it's a function
     if (typeof(messages[request.label]) === "function") {
+        // Create a message object
         const message = new RequestMessage(request, sender, sendResponse)
+
+        // Call the correct specific message handler
         messages[request.label](message)
+
+        // If the message handler stored a result, return it
         if (!(typeof(message.result) === 'undefined' || message.result == null)) {
             return message.result
         }
     }
 }
 
+// Handler for when a tab changes, in this case the URL
 function tabChangeHandler(tabId, changeInfo, url) {
     if (changeInfo.url) {
+        // Log what tab and URL changed and send a tab_change message to the tab's content script
         console.log("Tab changed:",{id:tabId,url:changeInfo.url})
         chrome.tabs.sendMessage(tabId, {
             message: "tab_change",
@@ -162,7 +193,9 @@ function tabChangeHandler(tabId, changeInfo, url) {
     }
 }
 
+// Handler for when a tab uses the action button
 function actionClickHandler(tab) {
+    // Log what tab (with some info) and send a action message to the tab's content script
     console.log("Action pressed from tab:", {id:tab.id, index:tab.index, url:tab.url, title:tab.title})
     chrome.tabs.sendMessage(tab.id, {
         message: "action",
@@ -170,10 +203,14 @@ function actionClickHandler(tab) {
     })
 }
 
+// Contains the message handlers the service worker has.
+// In this case getName which returns the name of the logged in user.
+// This might get called multiple times until it fetches a logged in user
 var messages = {
     getName: function(message) {
         message.sendResponse({data: getUserName()})
     }
 }
 
+// Call the onload function
 onload()
--- js/pointsTracker.js
+++ js/pointsTracker.js
@@ -36,6 +36,7 @@ function PointTracker() {
     this._channel = new VariableWrapper(null)
     this._ktnpause = new VariableWrapper(false, "boolean")
 
+    // Getters and setters
     this.isSupport = function() {
         return this._support.get()
     }
@@ -136,6 +137,7 @@ function PointTracker() {
         return this._search.modify(value)
     }
 
+    // Displaying placeholder text
     this.getDisplay = function() {
         return this.e.get().placeholder
     }
@@ -144,53 +146,67 @@ function PointTracker() {
         this.e.get().placeholder = data
     }
 
+    // Displaying points
     this.displayPoints = function(data) {
         if (typeof(data) !== "number") {
             this.clearDisplay()
             return
         }
 
+        // If there is a cooldown, opt to showing the cooldown after the point count
         if (this.getTimer() > 0) {
             let update = `Updates in ${this.getTimer()}s`
             if (this.getWarn() > 0) {
+                // If there's a force warning active, show that instead.
                 update = `Force available in ${60 - this.getTimerUp()}s`
             }
             this.setDisplay(`${this.getPointName()}: ${this.getPoints()} | ${update}`)
         } else {
+            // Since there's no cooldown, display only the value
             this.setPoints(data)
             this.setDisplay(`${this.getPointName()}: ${data}`)
         }
     }
 
+    // The timer ticking running every second
     this.updateTimer = function() {
         if (!this.isSupport()) return
 
+        // Tick the timers, and the warn if active
         this.tickTimer()
         this.tickTimerUp()
         if (this.getWarn() > 0)
             this.tickWarn()
 
+        // If the timer is out, fetch the points again
         if (this.getTimer() <= 0) {
-            if (this.isReqon()) {
+            if (this.isReqon()) { // Make sure there's not already a request since it's running async
                 return
             }
 
+            // Set the 'mutex' so it won't request twice
             this.setReqon(true)
             this.fetchPoints(() => {
+                // Reset the timers when the fetch has finished
                 this.setTimer(600)
                 this.setTimerUp(0)
+                // Also reset the mutex
                 this.setReqon(false)
             })
         } else {
+            // Display the current point count
             this.displayPoints(this.getPoints())
         }
     }
 
+    // This is used in case we need to force an update, mainly used for the action button
     this.forceUpdate = function() {
+        // Make sure to not be in a request
         if (this.isReqon()) {
             return
         }
 
+        // We still have a cooldown for forced at 60s
         if (this.getTimerUp() >= 60) {
             this.setReqon(true)
             this.setTimer(0)
@@ -200,10 +216,12 @@ function PointTracker() {
                 this.setReqon(false)
             })
         } else {
+            // If there's a cooldown on the forced, activate the warning
             this.setWarn(3)
         }
     }
 
+    // Clears the display to the default placeholder, usually "Search" or other language equivalent
     this.clearDisplay = function() {
         this.setDisplay(this.getSearch())
     }
@@ -213,39 +231,55 @@ function PointTracker() {
         this.setTimeout(() => this.clearDisplay(), 5000)
     }
 
+    // This fetches the points from the streamelements api
     this.fetchPoints = async function(callback) {
+        // Make sure the current channel is supported, get out if not
         if (!this.isSupport()) return
 
         try {
+            // Fetch and await the point URL /kappa/v2/points/<channel id>/<username>
             const response = await fetch(`https://api.streamelements.com/kappa/v2/points/${this.getChannelName()}/${this.getUserName()}`)
+            // Make sure the request was successful
             if (!response.ok) {
+                // If not, display the reason
                 console.log("Failed to fetch points:", (response.statusText || "").length > 0 ? (`${response.status} - ${response.statusText}`) : (response.status))
+                // Reset points to 0
                 this.setPoints(0)
+                // Still make sure to use the callback, since it's not dependent on the success
                 if (typeof(callback) === "function") {
                     callback()
                 }
             } else {
+                // If fetch was successful, read the json fully and get the points
                 const json = await response.json()
                 const points = json.points
 
+                // Update the points variable
                 this.setPoints(points)
+                // Use the callback
                 if (typeof(callback) === "function") {
                     callback()
                 }
             }
         } catch (error) {
+            // If there was any error, log the error to console
             console.error("Points Fetch Failed:\n", error)
+            // Reset points to 0
             this.setPoints(0)
+            // Call the callback just like in the error above
             if (typeof(callback) === "function") {
                 callback()
             }
         }
     }
 
+    // Basically tells the caller if the user is logged in and detected as such
     this.isValid = function() {
         return typeof(this.getUserName()) === "string" && this.getUserName().length > 0
     }
 
+    // Marks the current channel as "supported" which means their bot is valid and points for the bot is enabled
+    // Takes the channel id, channel name, points name and a callback to call
     this.channelSupported = function(id, name, pname, callback) {
         window.channelName = name
         window.channelLink = location.href
@@ -260,6 +294,8 @@ function PointTracker() {
         }
     }
 
+    // Marks the current channel as "unsupported" which means either they aren't using a valid bot,
+    //   their bot has points disabled, or any other issue that might arise
     this.channelUnsupported = function(name, reason) {
         if (typeof(name) === "string") {
             if (reason != null) {
@@ -277,43 +313,62 @@ function PointTracker() {
         this.clearDisplay()
     }
 
+    // Fetches a new channel using the StreamElements api
     this.fetchChannel = async function(channelName, callback) {
         try {
+            // Fetch the channels API as /kappa/v2/channels/<channel name>
+            // What we want is the <channel id> and the <channel points name>
             let response = await fetch(`https://api.streamelements.com/kappa/v2/channels/${channelName}`)
+            // Make sure the request was successful
             if (!response.ok) {
+                // If not, get the reason
                 let reason = undefined
-                if (response.status != "404") {
+                if (response.status != "404") { // If it wasn't a 404, log the actual error code and an option error text
                     console.log("Channel Points Error", `${response.status}${(response.statusText || "").length > 0 ? (` - ${response.statusText}`) : ""}`)
-                } else {
+                } else { // If it was a 404, channel isn't using StreamElements
                     reason = "Channel not using StreamElements"
                 }
+                // Mark the channel as unsupported (this might be cached in the future)
                 this.channelUnsupported(channelName, reason)
             } else {
+                // Get the json if successful
                 const json = await response.json()
+                // Make sure the profile is valid
                 if (json.statusCode == "404" || json.provider !== "twitch") {
+                    // If it was 404 or provider isn't twitch, mark as unsupported with the reason provided
                     this.channelUnsupported(channelName, json.statusCode == "404" ? "StreamElements profile not found" : "wrong platform")
                 } else {
+                    // If it was valid, log the channel as supported
                     console.log("Supported Channel:",channelName)
+                    // Also fetch their loyalty setup, which is what points are referred to
                     response = await fetch(`https://api.streamelements.com/kappa/v2/loyalty/${json._id}`)
+                    // Make sure the request was successful
                     if (!response.ok) {
+                        // If not, log the status and mark channel as unsupported
                         console.log("Channel Points Error", `${response.status}${(response.statusText || "").length > 0 ? (` - ${response.statusText}`) : ""}`)
                         this.channelUnsupported(channelName)
                     } else {
+                        // If successful, read json fully
                         const loyaltyJson = await response.json()
+                        // Make sure there's a loyalty element, and that loyalty is enabled
                         if (loyaltyJson != null && loyaltyJson.loyalty != null && loyaltyJson.loyalty.enabled == true) {
+                            // Mark channel as supported, providing channel id, channel name, loyalty name (or default value if not set), and the callback
                             this.channelSupported(json._id, channelName, loyaltyJson.loyalty.name || "points", callback)
                         } else {
+                            // If not, mark as unsupported, providing reason as points being disabled
                             this.channelUnsupported(channelName, "Channel Points Disabled")
                         }
                     }
                 }
             }
         } catch (error) {
+            // If there was any exceptional error, log the error and mark unsupported
             console.log("Channel Points Error",error)
             this.channelUnsupported(channelName)
         }
     }
 
+    // Checks if the URL changed, and if so, re-fetches the new channel
     this.updateChannel = function(callback) {
         if (window.channelLink !== location.href) {
             let cname = location.href
@@ -323,27 +378,38 @@ function PointTracker() {
         }
     }
 
+    // Called externally when the tab or url changes
     this.channelUpdate = function() {
         this.updateChannel(() => {
+            // We're using forceUpdate for this, so make sure to forge the force-timer to be finished
             this.setTimerUp(61)
             this.forceUpdate()
         })
     }
 
+    // Searches for and if found, returns the search box element to the caller
     this.getElement = function() {
+        // If it already exists, return it
         if (typeof(this.e.get()) === "object" && this.e.get() != null) {
             return this.e.get()
         }
 
+        // Search for and fetch the value
         const result = Array.from(document.querySelectorAll("input[type=\"search\"]")).filter(this.filterElement)[0]
+
+        // Make sure it's a valid object
         if (typeof(result) === "object" && result != null) {
+            // IF so, set it, and return it
             this._search.set(result.placeholder)
             return this.e.modify(result)
         } else {
+            // Otherwise, simply return null
             return null
         }
     }
 
+    // The filter callback from the function just above, called ``getElement``
+    // It simply makes sure that the element is in the navbar's search box, and that it's not hidden (since the mobile version is there too but marked as hidden)
     this.filterElement = function(el) {
         const _el = el
         while(el.parentElement != el && (el = el.parentElement) != null) {
@@ -354,17 +420,22 @@ function PointTracker() {
         return false
     }
 
+    // Fetches the currently logged-in username from the service worker
     this.preInit = function() {
         chrome.runtime.sendMessage({ label: "getName" }, response => {
+            // Makes sure the response is valid
             if (typeof(response.data) === "string" && response.data.length > 0) {
+                // If so, set the username and now look for the search bar
                 this.setUserName(response.data)
                 setTimeout(() => this.waitForSearch(), 0)
             } else {
+                // If not, run this function again in 2 seconds
                 setTimeout(() => this.preInit(), 2000)
             }
         })
     }
 
+    // Searches for the search bar (no pun intended) until if finds it, and then calls ``postInit``
     this.waitForSearch = function() {
         const elem = this.getElement()
         if (typeof(elem) === "object" && elem != null) {
@@ -375,38 +446,51 @@ function PointTracker() {
         setTimeout(() => this.waitForSearch(), 1000)
     }
 
+    // The last initialization made upon first load, also has the main update loop, running every 1000ms
     this.postInit = function() {
+        // Sets support to false by default
         this.setSupport(false)
 
+        // Makes sure user is logged in (this might be uneccessary but keeping it just in case)
         if (!this.isValid()) {
             return
         }
 
+        // Sets the loop interval
         setInterval(() => {
             if (!this.isPaused()) {
                 this.updateTimer()
             }
         }, 1000)
 
+        // Fetches the channel
         this.channelUpdate()
     }
 }
 
+// Create an instance of the tracker
+// Using var here since it's running in an enclosed environment
 var tracker = new PointTracker()
 
+// The message handler reading messages from the service worker
 function messageHandler(request, sender, sendResponse) {
     if (request.message === "tab_change") {
+        // tab_change runs whenever the tab changes url, thus a channel fetch should be performed
         console.log("Url Changed:",request.url)
         tracker.channelUpdate()
     } else if (request.message === "action") {
+        // action runs whenever the user presses the action button to the right of the search bar,
+        //   thus the tracker should try to force a points update
         console.log("Force Update:",request.url)
         tracker.forceUpdate()
     }
 }
 
+// registers the message listener, and runs the preInit function
 function onload() {
     chrome.runtime.onMessage.addListener(messageHandler)
     setTimeout(() => tracker.preInit(), 0)
 }
 
+// Runs the onload function after 0ms, using a timeout so that the function runs "when browser is ready"
 setTimeout(onload, 0)
